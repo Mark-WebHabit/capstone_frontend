63421124aa8e33204156be7b26289faa3592f680
Mark-WebHabit <markbobis173@gmail.com>
2024-01-28 15:10:35 +0800

    fixed lawns color rendering

--- src/app/instance.js
+++ src/app/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:8080",
-  // baseURL: "https://holyangelsapi.onrender.com",
+  // baseURL: "http://localhost:8080",
+  baseURL: "https://holyangelsapi.onrender.com",
 });
